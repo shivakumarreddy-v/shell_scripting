11f94e666c7e1095bdfa1083c3d8fe8b17cab363
Shiva Vinjamoori <shivareddyvinj@gmail.com>
2026-05-17 09:02:08 -0500

    actions

---
+++ conversations.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "suresh:: Heloo Ramesh, How are you"
+echo "Ramesh:: Hi Suresh, I am fine thanks ofr asking. How are you doing?"
+echo "suresh:: doing fine. what are you learning ramesh?"
+echo "Ramesh:: iam learnig shell_scripting"
