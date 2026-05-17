9c01d07f8d0c8064db84d4ae3d3b27f764fa7137
Shiva Vinjamoori <shivareddyvinj@gmail.com>
2026-05-17 10:16:24 -0500

    actions

---
+++ time_stamp.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+TIMESTAMP=$(DATE)
+
+echo"script executed at: $TIMESTAMP"
---
+++ variable.sh
@@ -0,0 +1,8 @@
+
+person1=$!
+person2=$2
+
+echo "person1:: Heloo person2, How are you"
+echo "person2:: Hi person1, I am fine thanks ofr asking. How are you doing?"
+echo "person1:: doing fine. what are you learning ramesh?"
+echo "person2:: iam learnig shell_scripting"
