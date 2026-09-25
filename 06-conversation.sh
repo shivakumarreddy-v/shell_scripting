cf4f081f6cdebc5d2b3a32c39328f0eb991b9ba7
Shiva Vinjamoori <shivareddyvinj@gmail.com>
2026-09-24 20:09:04 -0500

    your message here

---
+++ 06-conversation.sh
@@ -0,0 +1,13 @@
+Num1=100
+Num2=shivakumar
+
+SUM=$(($Num1+Num2))
+
+echo "sum is: $SUM"
+
+
+fruits=("apple" "banana" "pomo")
+
+echo "fruits are: ${fruits[@]}"
+echo "first fruit is: ${fruits[0]}"
+echo "first fruit is: ${fruits[1]}"
